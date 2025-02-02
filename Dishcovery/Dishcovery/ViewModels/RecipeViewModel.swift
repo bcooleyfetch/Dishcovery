89e39b3ffd7322b67784920fedc33f3e1b9c2c50
bcooleyfetch <brad+fetch@cooleyweb.org>
2025-02-01 19:33:10 -0500

    MVP done

--- Dishcovery/Dishcovery/ViewModels/RecipeViewModel.swift
+++ Dishcovery/Dishcovery/ViewModels/RecipeViewModel.swift
@@ -8,4 +8,20 @@
 import Foundation
 
 @MainActor
-class RecipeViewModel: ObservableObject { }
+class RecipeViewModel: ObservableObject {
+  @Published var recipes: [Recipe] = []
+
+  private let apiService: APIServiceProtocol
+
+  init(apiService: APIServiceProtocol = APIService()) {
+    self.apiService = apiService
+    Task { await self.fetchRecipies()}
+  }
+
+  func fetchRecipies() async {
+    let fetchedRecipes = await self.apiService.fetchRecipies()
+    DispatchQueue.main.async {
+      self.recipes = fetchedRecipes
+    }
+  }
+}
--- Dishcovery/Dishcovery/Views/RecipeView.swift
+++ Dishcovery/Dishcovery/Views/RecipeView.swift
@@ -8,9 +8,57 @@
 import SwiftUI
 
 struct RecipeView: View {
+  @StateObject private var viewModel: RecipeViewModel = .init()
 
   var body: some View {
-    Text("Hello world!")
+    NavigationView {
+      List {
+        ForEach(viewModel.recipes) { recipe in
+          HStack {
+            AsyncImage(url: recipe.photoURLSmall) { phase in
+              if let image = phase.image {
+                image
+                  .resizable()
+                  .scaledToFit()
+              } else if phase.error != nil {
+                ZStack {
+                  Color.clear.background(.regularMaterial)
+                  Image(systemName: "exclamationmark.triangle.fill")
+                    .symbolRenderingMode(.multicolor)
+                }
+              } else {
+                ZStack {
+                  Color.clear.background(.regularMaterial)
+                  ProgressView()
+                }
+              }
+            }
+            .frame(width: 80, height: 80)
+            .cornerRadius(8)
+            VStack(alignment: .leading) {
+              Text(recipe.name)
+                .font(.headline)
+              Text(recipe.cuisine)
+                .font(.subheadline)
+            }
+            .padding(.leading)
+          }
+        }
+      }
+      .navigationTitle("Recipes")
+      .refreshable {
+        await self.viewModel.fetchRecipies()
+      }
+      .overlay {
+        if self.viewModel.recipes.isEmpty {
+          VStack {
+            Text("No recipes found.")
+            Text("Please try again later.")
+          }
+          .padding()
+        }
+      }
+    }
   }
 }
 
